4aeb9e6f4fa6f5d00e0399e866797ac65d9a6cda
Maciej Sznurawa <msznurawa@gmail.com>
2019-07-15 12:44:53 +0200

    Project v1

--- js/App.js
+++ js/App.js
@@ -1,12 +1,13 @@
 // OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
-}
+//nie jest potrzebna gdy korzytsamy z api - informację o id otrzymujemy z serwera i nie musimy go tworzyć losowo
+// function randomString() {
+// 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+// 	var str = '', i;
+// 	for (i = 0; i < 10; i++) {
+// 	  str += chars[Math.floor(Math.random() * chars.length)];
+// 	}
+// 	return str;
+// }
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -18,20 +19,35 @@ function generateTemplate(name, data, basicElement) {
   	return element;
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+//FUNKCJONALNOŚCI DLA API
+//zmienne do komunikacji z serwerem - url oraz nagłówki
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '4028',
+  'X-Auth-Token': '35aeda076ef954a8a84426508fefd03d'
+};
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+//zapytanie wysyłane do serwera
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+});
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+//funkcja generująca kolumny (tyle ile otrzymujemy w odpowiedzi serwera)
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+          var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+//funkcja generująca karty
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
